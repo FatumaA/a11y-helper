d3fd3ef6a98f504577a84c0eb4440d6d1293718d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-17 15:35:22 +0300

    feat: add auth skeleton view

--- chat-app/src/components/blocks/Header.tsx
+++ chat-app/src/components/blocks/Header.tsx
@@ -2,26 +2,44 @@ import React from "react";
 import { Button } from "../ui/button";
 import { navigate } from "astro/virtual-modules/transitions-router.js";
 
-const handleAuth = () => {
-	navigate("/auth");
-};
+const Header = ({ user }) => {
+	console.log("USERRRR", user);
+	const handleAuth = () => {
+		navigate("/auth");
+	};
 
-const Header = () => {
+	const handleSignOut = () => {};
+
+	const isSignedIn = user ? true : false;
 	return (
 		<div className="flex justify-between my-4 mx-8">
 			<h1>Logo</h1>
-			<div className="flex gap-4">
-				<Button className="cursor-pointer" onClick={handleAuth}>
-					Sign In
-				</Button>
-				<Button
-					className="cursor-pointer"
-					variant="outline"
-					onClick={handleAuth}
-				>
-					Sign Up
-				</Button>
-			</div>
+
+			{isSignedIn ? (
+				<div className="flex items-center gap-4">
+					<p>{user.email}</p>
+					<Button
+						className="cursor-pointer"
+						variant="outline"
+						onClick={handleSignOut}
+					>
+						Sign Out
+					</Button>
+				</div>
+			) : (
+				<div className="flex items-center gap-4">
+					<Button className="cursor-pointer" onClick={handleAuth}>
+						Sign In
+					</Button>
+					<Button
+						className="cursor-pointer"
+						variant="outline"
+						onClick={handleAuth}
+					>
+						Sign Up
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };
