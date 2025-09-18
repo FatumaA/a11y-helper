1fc3350eb6fb976b705b0bd898a6677a225aa1f0
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-18 23:31:55 +0300

    feat: add middleware for global route protection

---
+++ chat-app/src/middleware.ts
@@ -0,0 +1,34 @@
+import { defineMiddleware } from "astro:middleware";
+import { supabaseServerClient } from "./lib/supabase";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+	const { pathname } = context.url;
+
+	// Create Supabase server client for this request
+	const supabaseBE = supabaseServerClient({
+		request: context.request,
+		cookies: context.cookies,
+	});
+
+	// Get active user
+	const {
+		data: { user },
+	} = await supabaseBE.auth.getUser();
+
+	const isActiveUser = Boolean(user?.id && user?.email);
+
+	// protect /chat and its subroutes from unauthed access
+	if (pathname.startsWith("/chat")) {
+		if (!isActiveUser) {
+			return context.redirect("/");
+		}
+		return next();
+	}
+
+	// redirect authed users to chat from auth page
+	if (pathname === "/auth" && isActiveUser) {
+		return context.redirect("/chat");
+	}
+
+	return next();
+});
