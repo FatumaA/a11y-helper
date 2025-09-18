11b63ce6c0af6fb0c760767de2db2798979b4d9a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-18 15:25:51 +0300

    fix: add url state when navigating

--- chat-app/src/components/blocks/Header.tsx
+++ chat-app/src/components/blocks/Header.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Button } from "../ui/button";
 import { navigate } from "astro/virtual-modules/transitions-router.js";
 
+const enum AuthAction {
+	SIGN_IN = "sign-in",
+	SIGN_UP = "sign-up",
+}
+
 const Header = ({ user }) => {
 	console.log("USERRRR", user);
-	const handleAuth = () => {
-		navigate("/auth");
+	const handleAuth = (actionType: AuthAction) => {
+		navigate("/auth", { state: { actionType } });
 	};
 
 	const handleSignOut = () => {};
@@ -28,13 +33,16 @@ const Header = ({ user }) => {
 				</div>
 			) : (
 				<div className="flex items-center gap-4">
-					<Button className="cursor-pointer" onClick={handleAuth}>
+					<Button
+						className="cursor-pointer"
+						onClick={() => handleAuth(AuthAction.SIGN_IN)}
+					>
 						Sign In
 					</Button>
 					<Button
 						className="cursor-pointer"
 						variant="outline"
-						onClick={handleAuth}
+						onClick={() => handleAuth(AuthAction.SIGN_UP)}
 					>
 						Sign Up
 					</Button>
