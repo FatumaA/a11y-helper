017427655fd1b2aa01d8ce6d02f0efdf8f37e8cc
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-20 00:05:06 +0300

    chore: export new actions

--- chat-app/src/actions/index.ts
+++ chat-app/src/actions/index.ts
@@ -1,9 +1,13 @@
 import { magicAuth } from "./auth/magicAuth";
 import { signOut } from "./auth/signOut";
 import { generateResponse } from "./chat/generateLLMResponse";
+import { migrateChat } from "./chatDB/migrateChat";
+import { insertChatMsg } from "./chatMsgs/insertChatMsg";
 
 export const server = {
 	generateResponse,
 	magicAuth,
 	signOut,
+	migrateChat,
+	insertChatMsg,
 };
