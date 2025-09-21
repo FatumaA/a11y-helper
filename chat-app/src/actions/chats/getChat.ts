7e6b4e44893ac531135cfbf2e9c6eb5fb7efc670
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-21 21:50:04 +0300

    feat: make action to get individual chat

---
+++ chat-app/src/actions/chats/getChat.ts
@@ -0,0 +1,57 @@
+import { supabaseServerClient } from "@/lib/supabase";
+import { defineAction, type ActionAPIContext } from "astro:actions";
+import { z } from "astro:schema";
+import { type Database } from "../../../database.types";
+
+type Chat = Database["public"]["Tables"]["chats"]["Row"];
+
+type GetChatResult = {
+	success: boolean;
+	message: Chat | string;
+};
+
+const getChatById = async (
+	chatId: string,
+	context: ActionAPIContext
+): Promise<GetChatResult> => {
+	try {
+		const supabaseBE = supabaseServerClient({
+			request: context.request,
+			cookies: context.cookies,
+		});
+
+		const { error, data } = await supabaseBE
+			.from("chats")
+			.select("*")
+			.eq("id", chatId)
+			.single();
+
+		if (error) {
+			console.log("ERROR FINDING CHAT:", error);
+			return {
+				success: false,
+				message: "Error getting from chat table",
+			};
+		} else {
+			console.log("GET CHAT BY ID", data);
+			return {
+				success: true,
+				message: data,
+			};
+		}
+	} catch (error) {
+		console.log("READ CHATS ERROR:", error);
+		return {
+			success: false,
+			message: "Error getting from chat table",
+		};
+	}
+};
+
+export const getChat = defineAction({
+	input: z.object({ chatId: z.string() }),
+	handler: async (input, context) => {
+		const { chatId } = input;
+		return getChatById(chatId, context);
+	},
+});
