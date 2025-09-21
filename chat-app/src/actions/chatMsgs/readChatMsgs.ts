c86242c3b38e7e81123bf9ca6709f40d60f0541d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-21 22:00:07 +0300

    feat: make action to read chat thread

---
+++ chat-app/src/actions/chatMsgs/readChatMsgs.ts
@@ -0,0 +1,60 @@
+import { supabaseServerClient } from "@/lib/supabase";
+import { defineAction, type ActionAPIContext } from "astro:actions";
+import { z } from "astro:schema";
+import { type Database } from "../../../database.types";
+
+type ChatMessage = Database["public"]["Tables"]["chat_messages"]["Row"];
+
+type ReadChatMsgsResult = {
+	success: boolean;
+	message: ChatMessage[] | string;
+};
+
+// read all chat msgs in a particular chat
+const readMsgs = async (
+	activeChatId: string,
+	context: ActionAPIContext
+): Promise<ReadChatMsgsResult> => {
+	try {
+		const supabaseBE = supabaseServerClient({
+			request: context.request,
+			cookies: context.cookies,
+		});
+
+		const { data, error } = await supabaseBE
+			.from("chat_messages")
+			.select("*")
+			.eq("chat_id", activeChatId)
+			.order("created_at", { ascending: true });
+
+		if (error) {
+			console.log("ERROR READING FROM CHAT MSGS TABLE:", error);
+			return {
+				success: false,
+				message: "Error reading from chat msgs table",
+			};
+		} else {
+			console.log("READ CHAT MSGS", data);
+			return {
+				success: true,
+				message: data,
+			};
+		}
+	} catch (error) {
+		console.log("READ CHAT MSGS ERROR:", error);
+		return {
+			success: false,
+			message: "Error reading from chat msgs table",
+		};
+	}
+};
+
+export const readChatMsgs = defineAction({
+	input: z.object({
+		activeChatId: z.string(),
+	}),
+	handler: async (input, context) => {
+		const { activeChatId } = input;
+		return readMsgs(activeChatId, context);
+	},
+});
