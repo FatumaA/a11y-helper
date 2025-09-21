d1a087d29cb751130a46e4b21dfbafcc89b531fe
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-22 00:18:33 +0300

    chore: update exported actions in index

--- chat-app/src/actions/index.ts
+++ chat-app/src/actions/index.ts
@@ -3,6 +3,10 @@ import { signOut } from "./auth/signOut";
 import { generateResponse } from "./chat/generateLLMResponse";
 import { migrateChat } from "./chatDB/migrateChat";
 import { insertChatMsg } from "./chatMsgs/insertChatMsg";
+import { readChatMsgs } from "./chatMsgs/readChatMsgs";
+import { deleteChat } from "./chats/CRUD/deleteChat";
+import { readChats } from "./chats/CRUD/readChat";
+import { getChat } from "./chats/getChat";
 
 export const server = {
 	generateResponse,
@@ -10,4 +14,8 @@ export const server = {
 	signOut,
 	migrateChat,
 	insertChatMsg,
+	getChat,
+	readChatMsgs,
+	readChats,
+	deleteChat,
 };
