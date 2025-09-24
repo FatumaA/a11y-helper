5ba788c307f8a271ace089c131bc9e9f8b458b7b
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-09-25 01:28:18 +0300

    chore: update actions index file with all imports

--- chat-app/src/actions/index.ts
+++ chat-app/src/actions/index.ts
@@ -1,13 +1,34 @@
 import { magicAuth } from "./auth/magicAuth";
 import { signOut } from "./auth/signOut";
 import { generateResponse } from "./chat/generateLLMResponse";
-import { migrateChat } from "./chatDB/migrateChat";
+import { readChats } from "./chats/CRUD/readChat";
+import { migrateChat } from "./chats/migrateChat";
 import { insertChatMsg } from "./chatMsgs/insertChatMsg";
+import { createChat } from "./chats/CRUD/createChat";
+import { updateChatMsg } from "./chatMsgs/updateChatMsg";
+import { deleteChat } from "./chats/CRUD/deleteChat";
+import { readChatMsgs } from "./chatMsgs/readChatMsgs";
+import { updateChat } from "./chats/CRUD/updateChat";
+import { getChat } from "./chats/getChat";
+import { deleteAccount } from "./auth/deleteAccount";
+import { deleteChats } from "./chats/CRUD/deleteChats";
 
 export const server = {
 	generateResponse,
+	// auth
+	deleteAccount,
 	magicAuth,
 	signOut,
+	// chats
 	migrateChat,
+	getChat,
+	createChat,
+	readChats,
+	updateChat,
+	deleteChat,
+	deleteChats,
+	// chat messages
 	insertChatMsg,
+	readChatMsgs,
+	updateChatMsg,
 };
